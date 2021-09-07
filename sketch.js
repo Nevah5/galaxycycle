fbbc70afb02ea6d7b85991bb7ff0d7997c816328
Noah-5555 <noah.d.geeler@gmail.com>
2021-09-07 23:08:22 +0200

    added an alpha fade effect to the mountains

--- sketch.js
+++ sketch.js
@@ -25,20 +25,21 @@ function backgroundcol(){
 }
 
 function mountain(){
+    let alpha = 255 - (100 / width * mouseX);
     noStroke();
     //first mountain
-    fill(250, 237, 203);
+    fill(250, 237, 203, alpha);
     triangle(width*-0.1,height, width/5.6,height/4*2.8, 0,height);
-    fill(243, 213, 133);
+    fill(243, 213, 133, alpha);
     triangle(0,height, width/5.6,height/4*2.8, width/2.7,height);
     //second mountain
-    fill(250, 237, 203);
+    fill(250, 237, 203, alpha);
     triangle(width/6,height, width/2,height/2, width/3,height);
-    fill(243, 213, 133);
+    fill(243, 213, 133, alpha);
     triangle(width/3,height, width/2,height/2, width/3*2.5,height);
     //third mountain
-    fill(250, 237, 203);
+    fill(250, 237, 203, alpha);
     triangle(width/3*1.7,height, width/6*5,height/6*5, width/3*2.1,height);
-    fill(243, 213, 133);
+    fill(243, 213, 133, alpha);
     triangle(width/3*2.1,height, width/6*5,height/6*5, width,height);
 }
