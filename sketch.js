2ac54fe2ee1f7dc2f28b89ddf1be8fd931d1f8a1
Noah-5555 <noah.d.geeler@gmail.com>
2021-09-07 23:04:51 +0200

    added mountains and removed changelog

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 const color1 = [224, 195, 252];
 const color2 = [0, 24, 69];
-let cur_color = [
+let bgcolor = [
     (color1[0] + color2[0]) / 2,
     (color1[1] + color2[1]) / 2,
     (color1[2] + color2[2]) / 2,
@@ -11,13 +11,34 @@ function setup() {
 }
 
 function draw(){
-    background(cur_color[0], cur_color[1], cur_color[2]);
+    background(bgcolor[0], bgcolor[1], bgcolor[2]);
 
-    col_calc();
+    backgroundcol();
+
+    mountain();
 }
 
-function col_calc(){
-    cur_color[0] = color2[0] + ((color1[0] - color2[0]) / width * mouseX);
-    cur_color[1] = color2[1] + ((color1[1] - color2[1]) / width * mouseX);
-    cur_color[2] = color2[2] + ((color1[2] - color2[2]) / width * mouseX);
+function backgroundcol(){
+    bgcolor[0] = color2[0] + ((color1[0] - color2[0]) / width * mouseX);
+    bgcolor[1] = color2[1] + ((color1[1] - color2[1]) / width * mouseX);
+    bgcolor[2] = color2[2] + ((color1[2] - color2[2]) / width * mouseX);
+}
+
+function mountain(){
+    noStroke();
+    //first mountain
+    fill(250, 237, 203);
+    triangle(width*-0.1,height, width/5.6,height/4*2.8, 0,height);
+    fill(243, 213, 133);
+    triangle(0,height, width/5.6,height/4*2.8, width/2.7,height);
+    //second mountain
+    fill(250, 237, 203);
+    triangle(width/6,height, width/2,height/2, width/3,height);
+    fill(243, 213, 133);
+    triangle(width/3,height, width/2,height/2, width/3*2.5,height);
+    //third mountain
+    fill(250, 237, 203);
+    triangle(width/3*1.7,height, width/6*5,height/6*5, width/3*2.1,height);
+    fill(243, 213, 133);
+    triangle(width/3*2.1,height, width/6*5,height/6*5, width,height);
 }
