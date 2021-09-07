4b2cd2abd02739d2da877bf253916f83ef401b33
Noah-5555 <noah.d.geeler@gmail.com>
2021-09-07 17:31:14 +0200

    fade done

--- sketch.js
+++ sketch.js
@@ -1,5 +1,23 @@
+const color1 = [255, 203, 242];
+const color2 = [192, 253, 255];
+let cur_color = [
+    (color1[0] + color2[0]) / 2,
+    (color1[1] + color2[1]) / 2,
+    (color1[2] + color2[2]) / 2,
+];
+
 function setup() {
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
+    background(cur_color[0], cur_color[1], cur_color[2]);
+
+    col_calc();
+}
+
+function col_calc(){
+    cur_color[0] = color2[0] + ((color1[0] - color2[0]) / width * mouseX);
+    cur_color[1] = color2[1] + ((color1[1] - color2[1]) / width * mouseX);
+    cur_color[2] = color2[2] + ((color1[2] - color2[2]) / width * mouseX);
 }
